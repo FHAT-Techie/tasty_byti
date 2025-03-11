e6b927f0f4d88b11e8287c0d0d00fde87de27f9e
Chuks Omeifeukwu <chuksomeifeukwu@gmail.com>
2025-03-11 07:49:52 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './', // ✅ This ensures relative paths work
   build: {
     outDir: 'dist'
-  },
-  server: {
-    historyApiFallback: true
   }
 });
